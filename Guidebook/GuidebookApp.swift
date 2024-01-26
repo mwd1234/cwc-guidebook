3a86a3c531e4df05a0c597911f5599f01b831471
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-26 16:52:01 -0700

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Matthew Dembinski on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
